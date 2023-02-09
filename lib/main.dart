d3d00c9054eb3db0e3632b92cf26b610e8e8b71c
DSankaram <sankaram@octaloop.com>
2023-02-09 18:11:33 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_bot_chatgpt/chat_Screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChatGPT Bot',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
+
+
